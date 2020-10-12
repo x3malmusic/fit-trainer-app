d94eda212b4622ab269d4c3e8c8ac61f96caa04c
x3malmusic <x3malmusic@gmail.com>
2020-10-12 15:48:16 +0300

    user model added

---
+++ backend/models/User.js
@@ -0,0 +1,10 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  email: { type: String, required: [true, "Email is required"], unique: true },
+  password: { type: String, required: [true, "Password is required"] },
+  exercises: [{ type: mongoose.Types.ObjectId, ref: "Exercise" }],
+  workouts: [{type: mongoose.Types.ObjectId, ref: "Workout" }]
+});
+
+export const User = model("User", userSchema);
