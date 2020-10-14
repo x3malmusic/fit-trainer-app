821fcd935adba26f7db02eea1923113432c7e99e
x3malmusic <x3malmusic@gmail.com>
2020-10-14 12:38:31 +0300

    Dashboard calendar

--- frontend/src/views/Dashboard.js
+++ frontend/src/views/Dashboard.js
@@ -2,6 +2,8 @@ import React from "react";
 // @material-ui/core
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
+import InfiniteCalendar from 'react-infinite-calendar';
+import 'react-infinite-calendar/styles.css';
 
 
 export default function Dashboard() {
@@ -9,7 +11,7 @@ export default function Dashboard() {
     <div>
       <GridContainer>
         <GridItem xs={12} sm={6} md={3}>
-          Dashboard
+          <InfiniteCalendar width={400} height={400} />
         </GridItem>
       </GridContainer>
     </div>
