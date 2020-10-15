c9c2e082115d6fc11d6396f69711fb4e4b298ab9
x3malmusic <x3malmusic@gmail.com>
2020-10-15 09:51:50 +0300

    proper link to User

--- backend/models/Exercise.js
+++ backend/models/Exercise.js
@@ -1,9 +1,9 @@
-import { Schema, model } from "mongoose";
+import mongoose, { Schema, model } from "mongoose";
 
 const exerciseSchema = new Schema({
   name: {type: String},
   measureType: {type: String},
-  owner: {type: String},
+  owner: {type: mongoose.Types.ObjectId, ref: "User" },
 });
 
 export const Exercise = model("Exercise", exerciseSchema);
