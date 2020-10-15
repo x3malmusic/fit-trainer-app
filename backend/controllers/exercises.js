28e678f568be1c0de44f04049bdc0bf6f0540618
x3malmusic <x3malmusic@gmail.com>
2020-10-15 09:52:37 +0300

    create exercise by userId

--- backend/controllers/exercises.js
+++ backend/controllers/exercises.js
@@ -2,10 +2,10 @@ import {asyncHandler} from '../middlewares/async'
 import { Exercise } from '../models/Exercise'
 
 export const createExercise = asyncHandler(async (req, res, next) => {
-  const { email } = req.user
+  const { userId } = req.user
   const { name, measureType } = req.body
 
-  const exercise = new Exercise({name , measureType, owner: email})
+  const exercise = new Exercise({name , measureType, owner: userId})
   await exercise.save()
 
   res.status(200).send()
