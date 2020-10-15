b4d83251c565b2c5192235cea848e098e7a00020
x3malmusic <x3malmusic@gmail.com>
2020-10-15 11:08:11 +0300

    exercises populate

--- backend/controllers/exercises.js
+++ backend/controllers/exercises.js
@@ -1,12 +1,17 @@
 import {asyncHandler} from '../middlewares/async'
 import { Exercise } from '../models/Exercise'
+import { User } from "../models/User";
 
 export const createExercise = asyncHandler(async (req, res, next) => {
-  const { userId } = req.user
+  const { email, userId } = req.user
   const { name, measureType } = req.body
 
   const exercise = new Exercise({name , measureType, owner: userId})
   await exercise.save()
+
+  const user = await User.findOne({email})
+  user.exercises.push(exercise.id)
+  user.save()
 
   res.status(200).send()
 })
