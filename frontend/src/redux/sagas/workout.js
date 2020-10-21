e9a845604be2b3c2e258221deb4168b1c9f12b4b
x3malmusic <x3malmusic@gmail.com>
2020-10-21 11:27:02 +0300

    edit workout saga

--- frontend/src/redux/sagas/workout.js
+++ frontend/src/redux/sagas/workout.js
@@ -1,7 +1,12 @@
 import { takeLatest, put } from "redux-saga/effects";
 import http from "../../services/http";
 import { getToken } from "../../services/token";
-import { ADD_WORKOUT, SET_ERROR } from "../actions/types";
+import {
+  ADD_WORKOUT,
+  SET_ERROR,
+  UPDATE_WORKOUT,
+  UPDATE_WORKOUT_STATE
+} from "../actions/types";
 
 
 const addWorkout = function* ({ payload }) {
@@ -12,7 +17,22 @@ const addWorkout = function* ({ payload }) {
   }
 };
 
+const updateWorkout = function* ({ payload }) {
+  try {
+    // console.log(payload)
+    yield http.put(`/api/workouts/${payload._id}`,{exercises: payload.exercises}, {headers: {'authorization' : `Bearer ${getToken()}`}});
+    const updatedWorkouts = yield payload.workouts.map(workout => {
+      if(workout._id === payload._id) return {...workout, exercises: payload.exercises}
+      else return workout
+    })
+    yield put({ type: UPDATE_WORKOUT_STATE, payload: updatedWorkouts });
+  } catch (e) {
+    yield put({ type: SET_ERROR, payload: e.response.data });
+  }
+};
+
 
 export default [
   takeLatest(ADD_WORKOUT, addWorkout),
+  takeLatest(UPDATE_WORKOUT, updateWorkout),
 ];
